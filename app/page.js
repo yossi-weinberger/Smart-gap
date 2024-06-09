3200a57f6ace3b6327b9a501cbd657291a19ee94
Yossi Weinberger <137178593+yossi-weinberger@users.noreply.github.com>
2024-06-09 07:36:16 +0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 // import Image from "next/image";
 import HeroSection from "@/components/heroSection/heroSection";
 import styles from "./page.module.css";
-import Header from "@/components/header/header";
+// import Header from "@/components/header/header";
 
 export default function Home() {
   return (
