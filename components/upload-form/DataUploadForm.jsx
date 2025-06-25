005a68905ac9832828d293740319d67044479ef5
Gal Ben David <gal.benda3@gmail.com>
2025-06-25 14:50:35 +0300

    Update styling

--- components/upload-form/DataUploadForm.jsx
+++ components/upload-form/DataUploadForm.jsx
@@ -120,40 +120,41 @@ export default function DataUploadForm() {
 
     return (
         <section className="files rtl-text">
-
             <div className="form-container">
                 <h2 className="feature-text">העלאת קבצים</h2>
                 <svg className="folder-shape" viewBox="0 0 1370 809" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M0.5 8.00001C0.5 3.85788 3.85787 0.5 8 0.5H1094.91C1097.05 0.5 1099.09 1.4161 1100.51 3.01723L1130.58 36.8457C1132.2 38.6603 1134.51 39.6986 1136.94 39.6986H1362C1366.14 39.6986 1369.5 43.0564 1369.5 47.1986V801C1369.5 805.142 1366.14 808.5 1362 808.5H7.99999C3.85785 808.5 0.5 805.142 0.5 801V8.00001Z" stroke="#CCCCCC" />
                 </svg>
 
-                <div className="form-header questionnaire-name-section">
-                    <h3 className="form-headline rtl-text"> שם השאלון</h3>
-                    <p className="description rtl-text">   כל הקבצים יישמרו תחת שם זה. (לדוגמא: מקצה א׳ 2025)</p>
-                    <input className="link-input" type="text" name='' placeholder='כתבו את שם השאלון' value='' onChange={handleChange} required />
+                <div className="forms">
+                    <div className="form-header questionnaire-name-section">
+                        <h3 className="form-headline rtl-text"> שם השאלון</h3>
+                        <p className="description rtl-text">   כל הקבצים יישמרו תחת שם זה. (לדוגמא: מקצה א׳ 2025)</p>
+                        <input className="link-input" type="text" name='' placeholder='כתבו את שם השאלון' value='' onChange={handleChange} required />
+                    </div>
+
+                    <KeyTables
+                        tables={tables}
+                        handleChange={handleChange}
+                        data={combinedData}
+                        clearTablesForm={clearTablesForm}
+                        onSubmit={handleSubmitTables}
+                        isLoadingTables={isLoadingTables}
+                    />
+
+                    <SurveyData surveyData={surveyData}
+                        handleChange={handleChange}
+                        surveyDataLoadingStatus={surveyDataLoadingStatus}
+                        clearSurveyForm={clearSurveyForm}
+                        onSubmit={handleSubmitSurveyData}
+                        isLoadingSurveyData={isLoadingSurveyData}
+                    />
+
+                    {combinedData && surveyDataLoadingStatus && (
+                        <div className="analyze-data-btn-container">
+                            <button className="btn load-btn analyze-data-btn" onClick={analyzeData}>ביצוע ניתוח נתונים</button>
+                        </div>)}
                 </div>
-
-                <KeyTables
-                    tables={tables}
-                    handleChange={handleChange}
-                    data={combinedData}
-                    clearTablesForm={clearTablesForm}
-                    onSubmit={handleSubmitTables}
-                    isLoadingTables={isLoadingTables}
-                />
-
-                <SurveyData surveyData={surveyData}
-                    handleChange={handleChange}
-                    surveyDataLoadingStatus={surveyDataLoadingStatus}
-                    clearSurveyForm={clearSurveyForm}
-                    onSubmit={handleSubmitSurveyData}
-                    isLoadingSurveyData={isLoadingSurveyData}
-                />
-
-                {combinedData && surveyDataLoadingStatus && (
-                    <div className="analyze-data-btn-container">
-                        <button className="btn load-btn analyze-data-btn" onClick={analyzeData}>ביצוע ניתוח נתונים</button>
-                    </div>)}
             </div>
         </section>
     )
